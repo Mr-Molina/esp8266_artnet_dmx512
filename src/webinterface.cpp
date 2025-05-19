f4e9858e02cf32c3228dfdc72e4cf2de145d4ee7
Mr. Molina <jmolina@myrccs.com>
2025-05-19 13:47:45 -0400

    Update webinterface.cpp

--- src/webinterface.cpp
+++ src/webinterface.cpp
@@ -5,48 +5,69 @@ extern ESP8266WebServer server;
 
 /***************************************************************************/
 
-static String getContentType(const String& path) {
-  if (path.endsWith(".html"))       return "text/html";
-  else if (path.endsWith(".htm"))   return "text/html";
-  else if (path.endsWith(".css"))   return "text/css";
-  else if (path.endsWith(".txt"))   return "text/plain";
-  else if (path.endsWith(".js"))    return "application/javascript";
-  else if (path.endsWith(".png"))   return "image/png";
-  else if (path.endsWith(".gif"))   return "image/gif";
-  else if (path.endsWith(".jpg"))   return "image/jpeg";
-  else if (path.endsWith(".jpeg"))  return "image/jpeg";
-  else if (path.endsWith(".ico"))   return "image/x-icon";
-  else if (path.endsWith(".svg"))   return "image/svg+xml";
-  else if (path.endsWith(".xml"))   return "text/xml";
-  else if (path.endsWith(".pdf"))   return "application/pdf";
-  else if (path.endsWith(".zip"))   return "application/zip";
-  else if (path.endsWith(".gz"))    return "application/x-gzip";
-  else if (path.endsWith(".json"))  return "application/json";
+static String getContentType(const String &path)
+{
+  if (path.endsWith(".html"))
+    return "text/html";
+  else if (path.endsWith(".htm"))
+    return "text/html";
+  else if (path.endsWith(".css"))
+    return "text/css";
+  else if (path.endsWith(".txt"))
+    return "text/plain";
+  else if (path.endsWith(".js"))
+    return "application/javascript";
+  else if (path.endsWith(".png"))
+    return "image/png";
+  else if (path.endsWith(".gif"))
+    return "image/gif";
+  else if (path.endsWith(".jpg"))
+    return "image/jpeg";
+  else if (path.endsWith(".jpeg"))
+    return "image/jpeg";
+  else if (path.endsWith(".ico"))
+    return "image/x-icon";
+  else if (path.endsWith(".svg"))
+    return "image/svg+xml";
+  else if (path.endsWith(".xml"))
+    return "text/xml";
+  else if (path.endsWith(".pdf"))
+    return "application/pdf";
+  else if (path.endsWith(".zip"))
+    return "application/zip";
+  else if (path.endsWith(".gz"))
+    return "application/x-gzip";
+  else if (path.endsWith(".json"))
+    return "application/json";
   return "application/octet-stream";
 }
 
 /***************************************************************************/
 
-bool defaultConfig() {
+bool defaultConfig()
+{
   Serial.println("defaultConfig");
-  
+
   config.universe = 1;
   config.channels = 512;
   config.delay = 25;
   return true;
 }
 
-bool loadConfig() {
+bool loadConfig()
+{
   Serial.println("loadConfig");
 
   File configFile = LittleFS.open("/config.json", "r");
-  if (!configFile) {
+  if (!configFile)
+  {
     Serial.println("Failed to open config file");
     return false;
   }
 
   size_t size = configFile.size();
-  if (size > 1024) {
+  if (size > 1024)
+  {
     Serial.println("Config file size is too large");
     return false;
   }
@@ -57,7 +78,8 @@ bool loadConfig() {
 
   JsonDocument root;
   DeserializationError error = deserializeJson(root, buf.get(), size);
-  if (error) {
+  if (error)
+  {
     Serial.println("Failed to parse config file");
     return false;
   }
@@ -69,7 +91,8 @@ bool loadConfig() {
   return true;
 }
 
-bool saveConfig() {
+bool saveConfig()
+{
   Serial.println("saveConfig");
   JsonDocument root;
 
@@ -78,11 +101,13 @@ bool saveConfig() {
   N_CONFIG_TO_JSON(delay, "delay");
 
   File configFile = LittleFS.open("/config.json", "w");
-  if (!configFile) {
+  if (!configFile)
+  {
     Serial.println("Failed to open config file for writing");
     return false;
   }
-  else {
+  else
+  {
     Serial.println("Writing to config file");
     serializeJson(root, configFile);
     configFile.close();
@@ -90,7 +115,8 @@ bool saveConfig() {
   }
 }
 
-void printRequest() {
+void printRequest()
+{
   String message = "HTTP Request\n\n";
   message += "URI: ";
   message += server.uri();
@@ -99,43 +125,57 @@ void printRequest() {
   message += "\nHeaders: ";
   message += server.headers();
   message += "\n";
-  for (uint8_t i = 0; i < server.headers(); i++ ) {
+  for (uint8_t i = 0; i < server.headers(); i++)
+  {
     message += " " + server.headerName(i) + ": " + server.header(i) + "\n";
   }
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   Serial.println(message);
 }
 
-void handleUpdate1() {
+void handleUpdate1()
+{
   server.sendHeader("Connection", "close");
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
   ESP.restart();
 }
 
-void handleUpdate2() {
-  HTTPUpload& upload = server.upload();
-  if (upload.status == UPLOAD_FILE_START) {
+void handleUpdate2()
+{
+  HTTPUpload &upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START)
+  {
     Serial.setDebugOutput(true);
     WiFiUDP::stopAll();
     Serial.printf("Update: %s\n", upload.filename.c_str());
     uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
-    if (!Update.begin(maxSketchSpace)) { //start with max available size
+    if (!Update.begin(maxSketchSpace))
+    { // start with max available size
       Update.printError(Serial);
     }
-  } else if (upload.status == UPLOAD_FILE_WRITE) {
-    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+  }
+  else if (upload.status == UPLOAD_FILE_WRITE)
+  {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+    {
       Update.printError(Serial);
     }
-  } else if (upload.status == UPLOAD_FILE_END) {
-    if (Update.end(true)) { //true to set the size to the current progress
+  }
+  else if (upload.status == UPLOAD_FILE_END)
+  {
+    if (Update.end(true))
+    { // true to set the size to the current progress
       Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-    } else {
+    }
+    else
+    {
       Update.printError(Serial);
     }
     Serial.setDebugOutput(false);
@@ -143,11 +183,13 @@ void handleUpdate2() {
   yield();
 }
 
-void handleDirList() {
+void handleDirList()
+{
   Serial.println("handleDirList");
   String str = "";
   Dir dir = LittleFS.openDir("/");
-  while (dir.next()) {
+  while (dir.next())
+  {
     str += dir.fileName();
     str += " ";
     str += dir.fileSize();
@@ -156,13 +198,16 @@ void handleDirList() {
   server.send(200, "text/plain", str);
 }
 
-void handleNotFound() {
+void handleNotFound()
+{
   Serial.print("handleNotFound: ");
   Serial.println(server.uri());
-  if (LittleFS.exists(server.uri())) {
+  if (LittleFS.exists(server.uri()))
+  {
     handleStaticFile(server.uri());
   }
-  else {
+  else
+  {
     String message = "File Not Found\n\n";
     message += "URI: ";
     message += server.uri();
@@ -171,7 +216,8 @@ void handleNotFound() {
     message += "\nArguments: ";
     message += server.args();
     message += "\n";
-    for (uint8_t i = 0; i < server.args(); i++) {
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
       message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }
     server.setContentLength(message.length());
@@ -179,52 +225,61 @@ void handleNotFound() {
   }
 }
 
-void handleRedirect(const char * filename) {
+void handleRedirect(const char *filename)
+{
   handleRedirect((String)filename);
 }
 
-void handleRedirect(String filename) {
+void handleRedirect(String filename)
+{
   Serial.println("handleRedirect: " + filename);
   server.sendHeader("Location", filename, true);
   server.setContentLength(0);
   server.send(302, "text/plain", "");
 }
 
-bool handleStaticFile(const char * path) {
+bool handleStaticFile(const char *path)
+{
   return handleStaticFile((String)path);
 }
 
-bool handleStaticFile(String path) {
+bool handleStaticFile(String path)
+{
   Serial.println("handleStaticFile: " + path);
-  String contentType = getContentType(path);            // Get the MIME type
-  if (LittleFS.exists(path)) {                            // If the file exists
-    File file = LittleFS.open(path, "r");                 // Open it
+  String contentType = getContentType(path); // Get the MIME type
+  if (LittleFS.exists(path))
+  {                                       // If the file exists
+    File file = LittleFS.open(path, "r"); // Open it
     server.setContentLength(file.size());
-    server.streamFile(file, contentType);               // And send it to the client
-    file.close();                                       // Then close the file again
+    server.streamFile(file, contentType); // And send it to the client
+    file.close();                         // Then close the file again
     return true;
   }
   Serial.println("\tFile Not Found");
-  return false;                                         // If the file doesn't exist, return false
+  return false; // If the file doesn't exist, return false
 }
 
-void handleJSON() {
+void handleJSON()
+{
   // this gets called in response to either a PUT or a POST
   Serial.println("handleJSON");
   printRequest();
 
-  if (server.hasArg("universe") || server.hasArg("channels") || server.hasArg("delay")) {
+  if (server.hasArg("universe") || server.hasArg("channels") || server.hasArg("delay"))
+  {
     // the body is key1=val1&key2=val2&key3=val3 and the ESP8266Webserver has already parsed it
     N_KEYVAL_TO_CONFIG(universe, "universe");
     N_KEYVAL_TO_CONFIG(channels, "channels");
     N_KEYVAL_TO_CONFIG(delay, "delay");
     handleStaticFile("/reload_success.html");
   }
-  else if (server.hasArg("plain")) {
+  else if (server.hasArg("plain"))
+  {
     // parse the body as JSON object
     JsonDocument root;
     DeserializationError error = deserializeJson(root, server.arg("plain").c_str(), server.arg("plain").length());
-    if (error) {
+    if (error)
+    {
       handleStaticFile("/reload_failure.html");
       return;
     }
@@ -233,7 +288,8 @@ void handleJSON() {
     N_JSON_TO_CONFIG(delay, "delay");
     handleStaticFile("/reload_success.html");
   }
-  else {
+  else
+  {
     handleStaticFile("/reload_failure.html");
     return; // do not save the configuration
   }
